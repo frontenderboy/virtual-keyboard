2c9b115f697367dc7e90d60ee9cfd1a6f5b150a5
Aliaksei Lahuta <frontenderboy@gmail.com>
2023-07-22 11:58:06 +0300

    feat: add unsupporting for mobile devices

--- js/index.js
+++ js/index.js
@@ -5,3 +5,20 @@ window.addEventListener('keydown', (event) => {
     keyboardBtn.classList.add('active-btn');
     event.preventDefault();
 })
+
+let isMobile = {
+	Android: function() {return navigator.userAgent.match(/Android/i);},
+	BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},
+	iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
+	Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
+	Windows: function() {return navigator.userAgent.match(/IEMobile/i);},
+	any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}
+};
+
+const keyboard = document.querySelector('.keyboard');
+const errorText = document.querySelector('.error-text');
+
+if(isMobile.any()) {
+    keyboard.classList.add('none');
+    errorText.classList.add('visible')
+}
